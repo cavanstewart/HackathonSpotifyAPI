4ec29216cffb32ef144a1eaf4258e36494f184f7
Cavan Stewart <cavanwstewart@gmail.com>
2019-10-28 13:46:08 -0700

    auth code added

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+
+class App extends Component {
+
+
+
+    componentDidMount() {
+
+      }
+    
+
+
+    render() {
+        return (
+            <div className="container">
+                Hello World!
+            </div>
+        )
+    }
+}
+
+export default App;
